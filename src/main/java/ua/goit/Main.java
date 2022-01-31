2a37029369df4bbd453614fbfa5de9dd4c38ffe8
alexyanov <yanov.alexander@gmail.com>
2022-01-31 21:38:41 +0200

    Add logic with find by id

--- src/main/java/ua/goit/Main.java
+++ src/main/java/ua/goit/Main.java
@@ -2,6 +2,9 @@ package ua.goit;
 
 import ua.goit.config.DatabaseManagerConnector;
 import ua.goit.config.PropertiesUtil;
+import ua.goit.dl.JobsRepository;
+import ua.goit.dl.Repository;
+import ua.goit.model.dao.JobsDao;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -12,5 +15,13 @@ public class Main {
         DatabaseManagerConnector dbConnector = new DatabaseManagerConnector(util.getHostname(), util.getPort(), util.getSchema(),
                 util.getUser(), util.getPassword());
         Connection connection = dbConnector.getConnection();
+        Repository<JobsDao> repository = new JobsRepository(connection);
+
+        JobsDao acAccount = repository.findById("AC_ACCOUNT");
+
+        System.out.println(acAccount.getJobId());
+        System.out.println(acAccount.getJobTitle());
+        System.out.println(acAccount.getMinSalary());
+        System.out.println(acAccount.getMaxSalary());
     }
 }
